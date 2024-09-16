bbfecd93d249831f8c3b90021ed5d6c21ce06ee7
Prabhav Sunil <psunil@ripple.com>
2024-09-16 16:42:21 +0530

    Initial commit

---
+++ terraform/argocd.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "4.9.7"
+  create_namespace = true
+  values = [file("argocd/values/argocd.yaml")]
+}
