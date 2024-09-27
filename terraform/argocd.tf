7acf93f5469b4ded0a53d9c29ea830122a8502f2
Prabhav Sunil <psunil@ripple.com>
2024-09-27 23:11:48 +0530

    ArgoCD updated to latest version

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -3,7 +3,17 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "4.9.7"
+  version          = "7.6.5"
   create_namespace = true
   values           = [file("argocd/values/argocd.yaml")]
+}
+
+resource "helm_release" "argocd-apps" {
+  name             = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  version          = "2.0.1"
+  create_namespace = true
+  values           = [file("argocd/values/argocd-apps.yaml")]
 }
